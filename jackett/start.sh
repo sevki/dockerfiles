d65a5d9a7178a32c27b529541221d1a0bc19a4c4
sevki <s@sevki.org>
2018-10-11 05:31:59 +0100

    jackett

--- jackett/start.sh
+++ jackett/start.sh
@@ -3,11 +3,10 @@
 ID=$(id -u)
 
 docker run -d \
-    --name sonarr \
+    --name jackett \
     --net=host \
     -e PUID=$ID \
     -e PGID=$ID \
     -v /etc/localtime:/etc/localtime:ro \
-    -v /mnt/extSyn/sonarr:/config \
-    -v '/mnt/extSyn/TV Shows':/tv  \
+    -v /mnt/extSyn/jackett:/config \
     linuxserver/sonarr
